90cb4ceed66abc0d0cdbadc90f51a4068aa47c09
Aleksei <aleksei.lapenkov@gmail.com>
2022-10-16 15:43:26 +0300

    Внес правки в фибоначчи

--- src/homework4/hard.java
+++ src/homework4/hard.java
@@ -1,18 +1,30 @@
 package homework4;
 
+import java.util.Scanner;
+
 public class hard {
     public static void main(String[] args) {
-        int n0 = 1;
-        int n1 = 1;
-        int n2;
-        System.out.print (n0+" "+n1+" ");
-        int[] arr = new int[10];
-        for (int i = 2; i < arr.length; i++) {
-            arr[i] = (n0+n1);
-            };
-        for (int i = 2; i < arr.length; i++){
-            System.out.print (arr[i] + " ");
-        }
+        System.out.println("Программа выводит последовательность фибонначи до F(n)");
+        System.out.println("Введите n: ");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.print("Последовательность фибоначчи до F(" + n + "):");
+        int fib0 = 0;
+        int fib1 = 1;
+        if (n >= 2) {
+            int[] fibNum = new int[n + 1];
+            fibNum[0] = fib0;
+            fibNum[1] = fib1;
+            System.out.print(fibNum[0] + " " + fibNum[1] + " ");
+            for (int i = 2; i <= n; i++) {
+                fibNum[i] = fibNum[i - 1] + fibNum[i - 2];
+                System.out.print(fibNum[i] + " ");
+            }
+        } else if (n == 1) {
+            System.out.println(fib0 + " " + fib1);
+        } else {
+            System.out.println(fib0);
         }
     }
+}
 
